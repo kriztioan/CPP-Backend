4c908190e551b60bb8baa40f553624927dbd192a
PAHdb <Christiaan.Boersma@nasa.gov>
2021-12-15 12:37:58 -0800

    Add code coverage and gh-pages

--- config.h
+++ config.h
@@ -2,5 +2,5 @@
   "NASA Ames PAH IR Spectroscopic Database\nDeveloped at the Astrophysics "    \
   "and Astrochemistry Laboratory at NASA Ames Research Center\nContact: "      \
   "Christiaan.Boersma@nasa.gov\n © 2006-2021"
-#define PROGRAM_VERSION "3.6"
-#define PROGRAM_BUILD "118"
+#define PROGRAM_VERSION "3.7"
+#define PROGRAM_BUILD "119"
